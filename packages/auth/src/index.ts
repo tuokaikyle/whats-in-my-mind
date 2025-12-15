d6b1a26b67fe7c7370770f09fe8b5bae750992c2
Kai <867367934@qq.com>
2025-12-15 17:30:19 +1000

    try setting up

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,23 +1,33 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@OverchargedList/db";
-import * as schema from "@OverchargedList/db/schema/auth";
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@OverchargedList/db';
+import * as schema from '@OverchargedList/db/schema/auth';
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "pg",
+  database: drizzleAdapter(db, {
+    provider: 'pg',
 
-		schema: schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: "none",
-			secure: true,
-			httpOnly: true,
-		},
-	},
+    schema: schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    },
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    },
+  },
 });
