566bffc48b7c8e72f1b1e2e111b03900f5209042
tuokaikyle <kai_tuo@outlook.com>
2026-01-18 14:20:16 +1000

    sidebar menu font no weight

--- apps/web/src/components/app-sidebar.tsx
+++ apps/web/src/components/app-sidebar.tsx
@@ -1,33 +1,43 @@
 import { UserButton } from '@daveyplate/better-auth-ui';
 import { Link, useNavigate } from '@tanstack/react-router';
 import { useEffect, useRef } from 'react';
-import { Command, Home, ListTodo } from 'lucide-react';
+import { Command } from 'lucide-react';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
 } from '@/components/ui/sidebar';
 import { queryClient } from '@/utils/trpc';
 import { authClient } from '@/lib/auth-client';
 
-const menuItems = [
-  {
-    title: 'Home',
-    url: '/',
-    icon: Home,
-  },
-  {
-    title: 'Todos',
-    url: '/todos',
-    icon: ListTodo,
-  },
-];
+const data = {
+  navMain: [
+    {
+      title: 'Home',
+      url: '/',
+    },
+    {
+      title: 'View',
+      items: [
+        {
+          title: 'Simple',
+          url: '/view/simple',
+        },
+        {
+          title: 'Advanced',
+          url: '/view/advanced',
+        },
+      ],
+    },
+  ],
+};
 
 export function AppSidebar() {
   const navigate = useNavigate();
@@ -49,16 +59,16 @@ export function AppSidebar() {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton
-              size="lg"
-              render={<a href="#" />}
-              className="rounded-md"
+              size='lg'
+              render={<Link to='/' />}
+              className='rounded-md'
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <Command className="size-4" />
+              <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+                <Command className='size-4' />
               </div>
-              <div className="flex flex-col gap-0.5 leading-none">
-                <span className="font-medium">Documentation</span>
-                <span className="">v1.0.0</span>
+              <div className='flex flex-col gap-0.5 leading-none'>
+                <span className='font-medium'>Documentation</span>
+                <span className=''>v1.0.0</span>
               </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -66,31 +76,42 @@ export function AppSidebar() {
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {menuItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton
-                    className="rounded-md"
-                    render={<Link to={item.url} />}
-                  >
-                    <item.icon />
-                    {item.title}
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  className='font-medium rounded-md'
+                  render={<Link to={item.url}  />}
+                >
+                  {item.title}
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuButton
+                          className='font-medium rounded-md'
+                          render={<Link to={item.url} className='' />}
+                        >
+                          {item.title}
+                        </SidebarMenuButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
             <UserButton
-              size="default"
-              side="right"
+              size='default'
+              side='right'
               sideOffset={16}
-              className="w-full bg-primary-foreground text-primary hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
+              className='w-full bg-primary-foreground text-primary hover:bg-sidebar-accent hover:text-sidebar-accent-foreground'
             />
           </SidebarMenuItem>
         </SidebarMenu>
