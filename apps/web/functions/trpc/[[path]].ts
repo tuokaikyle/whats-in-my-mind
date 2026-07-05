b94fa81b965a967330229d12f85e40372680f222
tuokaikyle <kai_tuo@outlook.com>
2026-07-06 00:57:02 +1000

    ds - fix login on mobile

---
+++ apps/web/functions/trpc/[[path]].ts
@@ -0,0 +1,17 @@
+const WORKER_URL = 'https://whats-in-my-mind-api.saline.workers.dev';
+
+export async function onRequest(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  const workerUrl = `${WORKER_URL}${url.pathname}${url.search}`;
+
+  return fetch(workerUrl, {
+    method: request.method,
+    headers: request.headers,
+    body:
+      request.method !== 'GET' && request.method !== 'HEAD'
+        ? request.body
+        : undefined,
+    redirect: 'manual',
+  });
+}
